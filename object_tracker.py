51528ff4756768353f45cbec2dc58e3ca97c204d
Tombriss <brisson.thomas@gmail.com>
2021-02-04 09:36:13 +0100

    test hyperparams

--- object_tracker.py
+++ object_tracker.py
@@ -34,7 +34,7 @@ flags.DEFINE_string('model', 'yolov4', 'yolov3 or yolov4')
 flags.DEFINE_string('video', './data/video/test.mp4', 'path to input video or set to 0 for webcam')
 flags.DEFINE_string('output', None, 'path to output video')
 flags.DEFINE_string('output_format', 'XVID', 'codec used in VideoWriter when saving video to file')
-flags.DEFINE_float('iou', 0.50, 'iou threshold')
+flags.DEFINE_float('iou', 0.45, 'iou threshold')
 flags.DEFINE_float('score', 0.50, 'score threshold')
 flags.DEFINE_boolean('dont_show', False, 'dont show video output')
 flags.DEFINE_boolean('info', False, 'show detailed info of tracked objects')
@@ -42,9 +42,9 @@ flags.DEFINE_boolean('count', False, 'count objects being tracked on screen')
 
 def main(_argv):
     # Definition of the parameters
-    max_cosine_distance = 0.4
+    max_cosine_distance = 0.1 # 0.4
     nn_budget = None
-    nms_max_overlap = 1.0
+    nms_max_overlap = 1.0 # 1.0
     
     # initialize deep sort
     model_filename = 'model_data/mars-small128.pb'
